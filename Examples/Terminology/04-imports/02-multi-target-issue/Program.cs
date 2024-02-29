6b4da2bd1a607cff1ff5470b081b608b2121c991
Jan Krivanek <jankrivanek@microsoft.com>
2024-02-29 14:40:22 +0100

    Format

--- Examples/Terminology/04-imports/02-multi-target-issue/Program.cs
+++ Examples/Terminology/04-imports/02-multi-target-issue/Program.cs
@@ -3,13 +3,12 @@
 
 namespace My
 {
-	public class Program
-	{
-		
-		public static void Main(string[] args)
-		{
-			// See https://aka.ms/new-console-template for more information
-			Console.WriteLine("Hello, World!");
-		}
-	}
+    public class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
 }
